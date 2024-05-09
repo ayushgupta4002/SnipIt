df4fe08de226b42ee5d7b1dd4e4009de3215ecd0
Ayush <ayush4002gupta@gmail.com>
2024-05-09 17:52:16 +0530

    DB changed

--- snipit-client/src/firebaseConfig.js
+++ snipit-client/src/firebaseConfig.js
@@ -4,13 +4,29 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
+
+//dev database
+
+// const firebaseConfig = {
+//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+//   authDomain: "snipshelf.firebaseapp.com",
+//   projectId: "snipshelf",
+//   storageBucket: "snipshelf.appspot.com",
+//   messagingSenderId:process.env.NEXT_PUBLIC_FIREBASE_MSG_ID,
+//   appId: process.env.NEXT_PUBLIC_FIREBASE_APPID
+// };
+
+
+//production database
+
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
-  authDomain: "snipshelf.firebaseapp.com",
-  projectId: "snipshelf",
-  storageBucket: "snipshelf.appspot.com",
-  messagingSenderId:process.env.NEXT_PUBLIC_FIREBASE_MSG_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APPID
+  apiKey: process.env.PROD_FIREBASE_APIKEY,
+  authDomain: "snipit-e564b.firebaseapp.com",
+  projectId: "snipit-e564b",
+  storageBucket: "snipit-e564b.appspot.com",
+  messagingSenderId:process.env.PROD_FIREBASE_MSG_ID,
+  appId: process.env.PROD_FIREBASE_APPID
 };
 
 // Initialize Firebase
